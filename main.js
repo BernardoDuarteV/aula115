0de424cf9d440fc5526b2695e2bf9d84d8e849c7
BernardoDuarteV <128868900+BernardoDuarteV@users.noreply.github.com>
2023-11-13 19:46:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
+nosex = 0;
+nosey = 0;
+
 function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
 function setup() {
-canvas = createCanvas(640, 480);
-canvas.position(110, 250);
+canvas = createCanvas(300, 300);
+canvas.center();
 video = createCapture(VIDEO);
+video.size(300, 300);
 video.hide();
 
-tintColor = "";
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded() {
+console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+  if(results.length > 0) {
+  console.log(results);
+  nosex = results[0].pose.nose.x-15;
+  nosey = results[0].pose.nose.y-15;
+  }
+  }
+
 function draw() {
-image(video, 0, 0, 640, 480);
-tint(tintColor);
+image(video, 0, 0, 300, 300);
+image(clown_nose, nosex, nosey, 30, 30);
 }
 
 function takeSnapshot() {
-save('studentName.png');
+  save('myFilterImage.png');
 }
 
-function filterTint() {
-tintColor = document.getElementById("colorName").value;
-
-}
